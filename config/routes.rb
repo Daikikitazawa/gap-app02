173859468b481d97ae1d125cba46fe1eb1a9ba96
Daiki Kitazawa <dysoooooooon@gmail.com>
2021-06-02 23:16:54 +0900

    6 - heroku デプロイ用意

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+
   get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/" => "home#top"
   get "about" => "home#about"
+
+
+  resources :blogs
+     root "home#top"
 
 end
