28878ec606202c2fc050706a0d9a3772a70acff6
Daiki Kitazawa <dysoooooooon@gmail.com>
2021-06-03 22:19:49 +0900

    8 - posts/show作成 ＆ @id表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
-
-  get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "posts/index" => "posts#index"
+  get "posts/:id" => "posts#show"
+
   get "/" => "home#top"
   get "about" => "home#about"
 
