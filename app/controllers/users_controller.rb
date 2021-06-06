79f7d9c695832f1d0ddd42659cb1a897e1e05e92
Daiki Kitazawa <dysoooooooon@gmail.com>
2021-06-06 14:07:48 +0900

    22 - users#new 新規登録機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,13 @@ class UsersController < ApplicationController
   end
 
   def new
+    @users = User.new
+  end
+
+  def create
+    @users = User.new(name: params[:name], email: params[:email], password_digest: params[:password_digest])
+    @users.save
+    redirect_to("/users/index")
   end
 
 end
