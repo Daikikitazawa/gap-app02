dcbab1dd8ffbd806bbb82ea3a94bde94cc94b550
Daiki Kitazawa <dysoooooooon@gmail.com>
2021-06-06 13:38:54 +0900

    21 - users#new 修正＆ヘッダーリンク設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "users/index" => "users#index"
-  get "users/new" => "users#new"
+  get "signup" => "users#new"
 
 
   get "users/:id" => "users#show"
