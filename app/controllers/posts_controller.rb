5ea902f7838983720f0d1ca433adcb7112d0da41
Daiki Kitazawa <dysoooooooon@gmail.com>
2021-06-09 22:31:47 +0900

    36 - 投稿者のみ編集＆削除リンクを表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,13 @@
 class PostsController < ApplicationController
+  before_action :ensure_currect_user,{only: [:edit, :update, :destroy]}
+
+  def ensure_currect_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+end
 
   def index
     @posts = Post.all.order(created_at: :desc)
