4137d73a6ee04a850acf31312e6dcb5362a37b6e
Daiki Kitazawa <dysoooooooon@gmail.com>
2021-06-16 11:34:20 +0900

    45 - いいね機能の作成＆アイコン表示＆クリック時の色の変化

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "users/ranking" => "users#ranking"
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
 
   get "login" => "users#login_form"
   post "login" => "users#login"
